f0b57395cf22371a064d488800ec2fdd30029404
s3915155 <s3915155@rmit.edu.vn>
2023-04-05 21:59:51 +0700

    domain-feature: account class

---
+++ domain/account/Account.java
@@ -0,0 +1,37 @@
+package banking.domain.account;
+
+public class Account {
+    private final CardNumber cardNum;
+    private final PIN cardPIN;
+    private int balance;
+
+    public Account() {
+        this.cardNum = new CardNumber();
+        this.cardPIN = new PIN();
+        this.balance = 0;
+    }
+
+    @Override
+    public String toString() {
+        return this.cardNum + " " + this.cardPIN;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getCardNum() {
+        return cardNum.toString();
+    }
+
+
+    public String getCardPIN() {
+        return cardPIN.toString();
+    }
+
+
+}
